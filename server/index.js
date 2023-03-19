d1eeeae319ffc2fbca2b62d3dd10dd4e41d790dd
Ben <benawhiting@gmail.com>
2023-03-19 12:13:42 -0700

    Link to MongoDB

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
 
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to DB')
+  })
+  .catch((err) => console.log(err));
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on Port ${process.env.PORT}`)
 });
