1256edbe57a8d96235467efa9eec89b3d8686a95
Ben <benawhiting@gmail.com>
2023-03-25 16:32:28 -0700

    Add basic mapfrom Mapbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import Map from 'react-map-gl';
 
 const App = () => {
   return (
-    <div>Hello, World!</div>
+    <Map
+      initialViewState={{
+        longitude: -122.6784,
+        latitude: 45.5152,
+        zoom: 10
+      }}
+      style={{width: "50vw", height: "50vh"}}
+      mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      mapStyle="mapbox://styles/mapbox/streets-v9"
+    />
   );
 };
 
