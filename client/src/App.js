c119fb81cba6c4030d8500bfef886d3fa039b6bd
Ben <benawhiting@gmail.com>
2023-03-26 13:18:25 -0700

    Add marker icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Map, { Marker } from 'react-map-gl';
+import RoomIcon from '@mui/icons-material/Room';
 
 import './App.css';
 
@@ -16,8 +17,8 @@ const App = () => {
       mapStyle="mapbox://styles/mapbox/streets-v9"
     >
       <Marker longitude={-122.7784} latitude={45.5352} anchor="bottom" >
-      <div className='here'>You are here</div>
-    </Marker>
+        <RoomIcon style={{ color: "slateblue" }}/>
+      </Marker>
     </Map>
   );
 };
